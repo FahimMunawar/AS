07543e79f52b454c5a84ee47d732c387cc7784c6
Md. Munawar Hossain <50805668+FahimMunawar@users.noreply.github.com>
2025-01-08 13:10:03 +0600

    Deploy

--- pages/Video_How_To_Use.py
+++ pages/Video_How_To_Use.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+st.set_page_config(page_title="Image Classification", layout="wide")
 # Title for the page
 st.title("How to Use the Ankylosing Spondylitis Classification Website")
 
